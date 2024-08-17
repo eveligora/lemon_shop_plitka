5686d807788213b0f05ad41d93d2f0c7044f121c
vmikov <vmikov@ukr.net>
2024-08-17 17:51:39 +0300

    feat: adds the hero section

---
+++ src/js/hero.js
@@ -0,0 +1,12 @@
+new Swiper('.hero .swiper', {
+	direction: 'horizontal',
+	effect: 'fade',
+	// fadeEffect: {
+	// 	crossFade: true,
+	// },
+	loop: true,
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+});
