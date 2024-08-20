09ba36f5ec291586453b2b590a6c700d398b8cb9
Yevhenii <Yevhenii.Boliekhovskyi@promx.net>
2024-08-20 21:04:35 +0300

    feat: header bottom + header pos fixed + scrollFunction

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,16 @@
-const body = document.querySelector('body');
+const header = document.querySelector('.header');
+
+window.onscroll = function () {
+	scrollFunction();
+};
+
+function scrollFunction() {
+	if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+		header.classList.add('header--scroll');
+	} else {
+		header.classList.remove('header--scroll');
+	}
+}
 
 var swiper = new Swiper('.services_slider', {
 	pagination: {
