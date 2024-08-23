970ed39432a569b521ef3066ea9aab69d0da4099
Serhii_Zhovtobriukh <s.u.zhovtobriukh@gmail.com>
2024-08-23 21:17:08 +0300

    fixed: swiper horizontal

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,25 +1,26 @@
 const review = new Swiper('.swiper_review', {
-	
-	slidesPerView: 2,
+	direction: 'horizontal',
 	loop: true,
 	spaceBetween: 40,
 	navigation: {
 		nextEl: '.reviews__btn_right',
 		prevEl: '.reviews__btn_left',
 	  },
-	  noSwiping: true,
-	  noSwipingClass: 'noswip',
 	  breakpoints: {
-		
+		// when window width is >= 320px
 		320: {
-		  direction: 'vertical', 
-		  slidesPerView: 2,
+		  slidesPerView: 1,
+		  grid: {
+			rows: 2,
+		  },
 		},
-
+		// when window width is >= 480px
 		768: {
-		  direction: 'horizontal', 
 		  slidesPerView: 2,
-		}
+		  grid: {
+			rows: 1,
+		  },
+		},
 	  }
 	  
-});
+	  });
