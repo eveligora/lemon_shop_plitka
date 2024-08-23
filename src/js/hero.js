05c2faa76ff7779590d32ab097eb386c470f8b6a
Vitalii_A <svika96@gmail.com>
2024-08-23 11:20:54 +0300

    added js

--- src/js/hero.js
+++ src/js/hero.js
@@ -1,8 +1,11 @@
-new Swiper('.hero', {
-	direction: 'horizontal',
-	loop: true,
-	navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev',
-	},
-});
+
+if (window.location.pathname === '/index.html') {
+	new Swiper('.hero', {
+		direction: 'horizontal',
+		loop: true,
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+	});
+}
---
+++ src/js/price.js
@@ -0,0 +1,9 @@
+
+
+if (window.location.pathname === '/price.html') {
+    const tableBTN = document.querySelector(".table_price1__head"); 
+    const tableDATA = document.querySelector(".table_price1__data");
+    tableBTN.addEventListener("click", function() {
+        tableDATA.classList.toggle("active"); 
+    });
+}
