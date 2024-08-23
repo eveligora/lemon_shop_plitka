0db524b070c8363b8e0fb853437851d385e24226
Serhii_Zhovtobriukh <s.u.zhovtobriukh@gmail.com>
2024-08-23 18:02:51 +0300

    feat: review block

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,6 +1,25 @@
 const review = new Swiper('.swiper_review', {
-	direction: 'horizontal',
+	
 	slidesPerView: 2,
 	loop: true,
 	spaceBetween: 40,
+	navigation: {
+		nextEl: '.reviews__btn_right',
+		prevEl: '.reviews__btn_left',
+	  },
+	  noSwiping: true,
+	  noSwipingClass: 'noswip',
+	  breakpoints: {
+		
+		320: {
+		  direction: 'vertical', 
+		  slidesPerView: 2,
+		},
+
+		768: {
+		  direction: 'horizontal', 
+		  slidesPerView: 2,
+		}
+	  }
+	  
 });
