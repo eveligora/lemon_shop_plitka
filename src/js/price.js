3c1362e4f1405d5958d0b96e52a8890bd38846a8
Vitalii_A <svika96@gmail.com>
2024-08-25 10:53:32 +0300

    added second table + update JS fort all tables

--- src/js/price.js
+++ src/js/price.js
@@ -1,15 +1,22 @@
 // if (window.location.pathname === '/price.html') {
-	const tableBTN = document.querySelector('.table_price1_head');
-	const tableDATA = document.querySelector('.table_price1_data');
-	const arrow = document.querySelector('.price_img__arrow');
-if (tableBTN) {
-	tableBTN.addEventListener('click', function () {
-		tableDATA.classList.toggle('active');
+	const tableBTN = document.querySelectorAll('.table_price_head');
+	const tableDATA = document.querySelectorAll('.table_price_data');
+	const arrow = document.querySelectorAll('.price_img__arrow');
 
-		if (tableDATA.classList.contains('active')) {
-			arrow.classList.add('closed');
+if (tableBTN) {
+tableBTN.forEach(function(e, index) {
+    e.addEventListener('click', function() {
+        console.log(index);
+		
+		tableDATA[index].classList.toggle("active")
+
+		if (tableDATA[index].classList.contains('active')) {
+			arrow[index].classList.add('closed');
 		} else {
-			arrow.classList.remove('closed');
+			arrow[index].classList.remove('closed');
 		}
-	});
+
+
+    });
+});
 }
